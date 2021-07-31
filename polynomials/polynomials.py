c01446c1f91842ac1cdc58df7a32a79345d53ac4
Alexander Green <amg220@ic.ac.uk>
2021-07-31 23:13:27 +0200

    Linter

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,14 +1,15 @@
 from numbers import Number
 
+
 class Polynomial:
     def __init__(self, coefs):
 
         self.coefficients = coefs
-    
+
     def degree(self):
 
         return len(self.coefficients) - 1
-    
+
     def __str__(self):
 
         coefs = self.coefficients
@@ -17,9 +18,9 @@ class Polynomial:
             terms.append(str(coefs[0]))
         if self.degree() and coefs[1]:
             terms.append(f"{'' if coefs[1] == 1 else coefs[1]}x")
-             
+          
         terms += [f"{'' if c == 1 else c }x^{d}"
-                 for d, c in enumerate(coefs[2:], start=2) if c ]
+                  for d, c in enumerate(coefs[2:], start=2) if c]
         
         return " + ".join(reversed(terms)) or "0"
 
@@ -29,28 +30,22 @@ class Polynomial:
 
     def __add__(self, other):
 
-        if isinstance(other, Polynomial):    
+        if isinstance(other, Polynomial): 
             common = min(self.degree(), other.degree()) + 1 
-            coefs = tuple(a + b for a, b in zip(self.coefficients, other.coefficients))
+            coefs = tuple(a + b for a, b in zip(self.coefficients,
+                          other.coefficients))
             coefs += self.coefficients[common:] + other.coefficients[common:]
-            
+
             return Polynomial(coefs)
-        
+
         elif isinstance(other, Number):
-            
-            return Polynomial((self.coefficients[0] + other,) 
-            + self.coefficients[1:])
+
+            return Polynomial((self.coefficients[0] + other,)
+                              + self.coefficients[1:])
 
         else:
             return NotImplemented
 
     def __radd__(self, other):
 
-        return self + other 
-
-
-
-
-
-
-
+        return self + other
