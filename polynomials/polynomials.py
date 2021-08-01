88f184067905254b6f6108967245a24936c54ba7
Alexander Green <amg220@ic.ac.uk>
2021-08-01 02:28:49 +0200

    added remainders

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -48,3 +48,47 @@ class Polynomial:
     def __radd__(self, other):
 
         return self + other
+
+    def __mul__(self, other):
+
+        if isinstance(other, Polynomial):
+            total = Polynomial((0,))
+            for n, c in enumerate(self.coefficients):
+                total += Polynomial(tuple(0 for i in range(n)) + tuple(c*v for v in other.coefficients))
+            return total
+
+        elif isinstance(other, Number):
+            return Polynomial(tuple(other * c for c in self.coefficients))
+
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+
+        return self * other
+
+    def __sub__(self, other):
+
+        return self + -1 * other
+
+    def __rsub__(self, other):
+
+        return other + -1 * self
+
+    def __mod__(self, other):
+
+        rem = self
+        i = 1
+        degdif = self.degree() - other.degree()
+        a = other.coefficients[-1]
+        while degdif >= i - 1 and other.degree():
+            rem -= (rem.coefficients[-i] / a) * Polynomial(tuple(0 for j in range(degdif + 1 - i)) + (1,)) * other
+            i += 1
+        return rem
+
+
+
+
+
+
+
--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -6,3 +6,9 @@ def test_print():
     p = Polynomial((2, 1, 0, 3))
 
     assert str(p) == "3x^3 + x + 2"
+
+    p = Polynomial((-42, 0, -12, 1))
+    q = Polynomial((-3, 1))
+
+    assert str(p % q) == "-123.0"
+
